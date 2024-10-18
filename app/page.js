5ebeaf708aea8957a15b542ae066d581d4a6b1e2
Arifkoyani <arifkoyani@gmail.com>
2024-10-18 22:23:05 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import AnimationOnScroll from "./animate/page"
+import Navbar from "./Navbar/page";
+import InfoBar from "./InfoBar/page"
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <InfoBar/>
+    <AnimationOnScroll/>   
+    
+    </>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['images.unsplash.com',"e7.pngegg.com","www.kisacoresearch.com","encrypted-tbn0.gstatic.com","remoteworksource.com"], // Allow Unsplash domain for next/image
+    },
+  };
+  
+  export default nextConfig;
+  
